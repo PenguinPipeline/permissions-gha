e3fd1b03f6af7e87d9350269db42a6a6e454a5a8
Mohit Ahuja <mohahuja@microsoft.com>
2021-04-27 11:09:25 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+import requests  # noqa We are just importing this to prove the dependency installed correctly
+
+
+def main():
+    # my_input = os.environ["INPUT_MYINPUT"]
+
+    result = f"Permissions File: {os.environ['permissions_file_path']}\n Reference File: {os.environ['restrictions_file_path']}"
+
+    print(f"::set-output name=result::{result}")
+
+
+if __name__ == "__main__":
+    main()
